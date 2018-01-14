653c01739390b1087da4e2cb6c2c9d47a0387706
Poornima Kumar <pkumar07@ufl.edu>
2018-01-14 23:41:31 +0530

    "Completed"

--- BouncyBalls/scripts/scripts.js
+++ BouncyBalls/scripts/scripts.js
@@ -1,8 +1,17 @@
 var canvas, ctx, w, h;
+var mousePos;
+
+
+var initialNumberOfBalls;
+var globalSpeedMultiplier = 1;
+var colorToEat = 'blue';
+var wrongBallsEaten = 0;
+var goodBallsEaten = 0;
+var numberOfGoodBalls;
 
 var player = {
   x:10,
-  y:10,
+  y:10, 
   width:20,
   height:20,
   color:'red'
@@ -14,28 +23,94 @@ var balls = [];
 window.onload = init;
 
 function init(){
+  console.log("came to init");
   canvas = document.querySelector("#myCanvas");
   ctx = canvas.getContext('2d');
   w = canvas.width;
   h = canvas.height;
-  
-  balls = createBalls(10);
+  console.log("Game is starting");
+  startGame(10);
+  console.log("Initializing mouseMove");
+  canvas.addEventListener('mousemove', mouseMoved);
   mainLoop();
 }
 
-function mainLoop(){
-  
-  ctx.clearRect(0,0,w,h);
-  
-  drawFilledRectangle(player);
-  
-  drawBalls(balls);
-  
-  moveBalls(balls);
+function startGame(nb){
+  do{
+    balls = createBalls(nb);
+    initialNumberOfBalls = nb;
+    numberOfGoodBalls = countNumberOfGoodBalls(balls, colorToEat);
+  }while(numberOfGoodBalls === 0);
+  console.log("Game started");
+  wrongBallsEaten = goodBallsEaten = 0;
+}
 
+function countNumberOfGoodBalls(balls, colorToEat){
+  var nb = 0;
+
+  balls.forEach(function(b){
+      if(b.color === colorToEat)
+        nb++;
+  });
+
+  return nb;
+}
+
+function changeNbBalls(nb){
+  startGame(nb);
+}
+
+function changeColorToEat(color){
+  colorToEat = color;
+}
+
+function changePlayerColor(color){
+  player.color = color;
+}
+
+function changeBallSpeed(coef){
+  globalSpeedMultiplier = coef;
+}
+
+function mouseMoved(evt){
+  mousePos = getMousePos(canvas,evt);
+}
+
+function getMousePos(canvas, evt){
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
+
+function movePlayerWithMouse(){
+  if(mousePos !== undefined){
+    player.x = mousePos.x;
+    player.y = mousePos.y;
+  }
+}
+
+function mainLoop(){
+  ctx.clearRect(0,0,w,h);
+ 
+  drawFilledRectangle(player);
+  drawBalls(balls);
+  drawBallNumbers(balls);
+  moveBalls(balls);
+  movePlayerWithMouse();
   requestAnimationFrame(mainLoop);
 }
 
+function circleRectOverlap(x0, y0, w0, h0, cx, cy, r) {
+  var testX=cx;
+  var testY=cy;
+  if (testX < x0) testX=x0;
+  if (testX > (x0+w0)) testX=(x0+w0);
+  if (testY < y0) testY=y0;
+  if (testY > (y0+h0)) testY=(y0+h0);
+  return (((cx-testX)*(cx-testX)+(cy-testY)*(cy-testY))< r*r);
+}
 
 function createBalls(n){
   var balls = [];
@@ -61,11 +136,54 @@ function getRandomColor(){
 }
 
 
+function drawBallNumbers(balls) {
+  ctx.save();
+  ctx.font="20px Arial";
+  
+  if(balls.length === 0) {
+    ctx.fillText("Game Over!", 20, 30);
+  } else if(goodBallsEaten === numberOfGoodBalls) {
+    ctx.fillText("You Win! Final score : " + (initialNumberOfBalls - wrongBallsEaten), 
+                 20, 30);
+  } else {
+    ctx.fillText("Balls still alive: " + balls.length, 210, 30);
+    ctx.fillText("Good Balls eaten: " + goodBallsEaten, 210, 50);
+     ctx.fillText("Wrong Balls eaten: " + wrongBallsEaten, 210, 70);
+  }
+  ctx.restore();
+}
+
 function drawBalls(balls){
   balls.forEach(function(ball){
     drawFilledCircle(ball);
   });
 }
+
+
+function moveBalls(balls){
+  balls.forEach(function(ball, index){
+    ball.x += ( ball.speedX * globalSpeedMultiplier);
+    ball.y += ( ball.speedY * globalSpeedMultiplier);
+    testCollisionWithWalls(ball);
+    testCollisionWithPlayer(ball, index);
+  });
+}
+
+
+
+function testCollisionWithPlayer(ball, index){
+  if(circleRectOverlap(player.x, player.y, player.width, player.height, ball.x, ball.y, ball.radius)){
+    if(ball.color === colorToEat){
+      goodBallsEaten += 1;
+    }
+    else{
+      wrongBallsEaten += 1;
+    }
+    balls.splice(index, 1);
+  }
+}
+
+
 
 function testCollisionWithWalls(b){
   if((b.x + b.radius) > w ){
@@ -88,16 +206,6 @@ function testCollisionWithWalls(b){
 }
 
 
-function moveBalls(balls){
-  balls.forEach(function(ball){
-    ball.x += ball.speedX;
-    ball.y += ball.speedY;
-    testCollisionWithWalls(ball);
-  });
-  
-  
-  
-}
 
 function drawFilledCircle(c){
   ctx.save();
